33ac1f20219bdd0ce517e60df3cb3b5d0b97af6c
einstein72 <mido.heb@gmail.com>
2015-01-17 12:03:38 +0200

    First commit

---
+++ Server/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+using System.IO;
+
+namespace Server
+{
+    public class Server
+    {
+        private TcpListener _listener;
+        private UInt16 _portnumber;
+        private IPAddress _ip;
+        
+    }
+
+    public class Connection
+    {
+        private Socket _connection;
+        private Stream _data;
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        
+
+        public Connection(ref Socket s)
+        {
+
+        }
+        
+    }
+}
